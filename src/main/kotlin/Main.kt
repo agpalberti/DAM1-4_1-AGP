050752cd5953e0aaf61834e330ad6aedf312a232
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-11-11 16:40:57 +0100

    Nuevos metodos
    ¿require en metodos?

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,47 +9,94 @@ class Modulo(maxAlumnos: Int) {
     var alumnos: Array<Alumno?> = arrayOfNulls<Alumno>(this.maxAlumnos)
     var evaluaciones: Array<Array<Float>> = Array(4) { (Array<Float>(this.maxAlumnos) { -1.0F }) }
 
-    companion object{
+    companion object {
         const val EV_PRIMERA = 0
         const val EV_SEGUNDA = 1
         const val EV_TERCERA = 2
         const val EV_FINAL = 3
     }
 
+    //Permite establecer la nota de cierto alumno y evaluación. Si se introduce correctamente los datos devuelve un true, si no, false. También devuelve false si el alumno no existe.
+    fun establecerNota(idAlumno: String, evaluacion: String, nota: Float): Boolean {
+        require(checkEvaluacion(evaluacion))
+        require(nota in 0.0..10.0)
+        val posicionAlumno = alumnos.indexOfFirst { it?.id == idAlumno }
+        if (posicionAlumno > -1 && checkEvaluacion(evaluacion) && nota in 0.0..10.0) {
+            evaluaciones[evaluacion.toInt()][posicionAlumno] = nota
+            return true
+        } else return false
+    }
+
+    //Establece la nota de la EV_FINAL para todos los alumnos mediante el promedio de las 3 primeras evaluaciones
+    fun calculaEvaluacionFinal() {
+        for (i in 0 until this.maxAlumnos) {
+            evaluaciones[EV_FINAL][i] =
+                (evaluaciones[EV_PRIMERA][i] + evaluaciones[EV_SEGUNDA][i] + evaluaciones[EV_TERCERA][i]) / 3
+        }
+    }
+
+    fun listaNotas(evaluacion: String): List<Pair<Float, Float>> {
+
+    }
+
+    //Devuelve un Int con el número de aprobados. Si la evaluación se introduce de forma incorrecta devuelve un -1.
+    fun numeroAprobados(evaluacion: String): Int {
+        if (checkEvaluacion(evaluacion)) {
+            return evaluaciones[evaluacion.toInt()].count { it >= 5 }
+        } else return -1
+    }
+
+    //Devuelve un Float con la nota más baja de una evaluación. Si se introduce incorrectamente la evaluación, devuelve un -1.0. Si la evaluación está vacía, devuelve un -2.0.
+    fun notaMasBaja(evaluacion: String): Float {
+        if (checkEvaluacion(evaluacion)) return evaluaciones[evaluacion.toInt()].minOrNull() ?: (-2.0F)
+        else return -1.0F
+    }
+
+    //Devuelve un Float con la nota más alta de una evaluación. Si se introduce incorrectamente la evaluación, devuelve un -1.0. Si la evaluación está vacía, devuelve un -2.0.
+    fun notaMasAlta(evaluacion: String): Float {
+        if(checkEvaluacion(evaluacion)) return evaluaciones[evaluacion.toInt()].maxOrNull()?:(-2.0F)
+        else return -1.0F
+    }
+
+    fun notaMedia(evaluacion: String): Float {}
+
+    fun hayAlumnosConDiez(evaluacion: String): Boolean {}
+
+    fun hayAlumnosAprobados(evaluacion: String): Boolean {}
+
+    fun primeraNotaNoAprobada(evaluacion: String): Float {}
+
+    fun listaNotasOrdenadas(evaluacion: String): List<Pair<Float, Float>> {}
+
     fun matricularAlumno(alumno: Alumno): Boolean {
-        val position = alumnos.indexOfFirst { it == null }
-        if (position > -1) {
-            alumnos[position] = alumno
+        val posicionAlumnoVacio = alumnos.indexOfFirst { it == null }
+        if (posicionAlumnoVacio > -1) {
+            alumnos[posicionAlumnoVacio] = alumno
             for (i in EV_PRIMERA..EV_FINAL) {
-                evaluaciones[i][position] = 0.0F
+                evaluaciones[i][posicionAlumnoVacio] = 0.0F
             }
             return true
         } else return false
     }
 
     fun bajaAlumno(idAlumno: String): Boolean {
-        val position = alumnos.indexOfFirst { it?.id == idAlumno }
-        if (position > -1) {
-            alumnos[position] = null
+        val posicionAlumno = alumnos.indexOfFirst { it?.id == idAlumno }
+        if (posicionAlumno > -1) {
+            alumnos[posicionAlumno] = null
             for (i in EV_PRIMERA..EV_FINAL) {
-                evaluaciones[i][position] = -1.0F
+                evaluaciones[i][posicionAlumno] = -1.0F
             }
             return true
         } else return false
     }
 
-    fun establecerNota(idAlumno: String, evaluacion: String, nota: Float): Boolean {
-        val position = alumnos.indexOfFirst { it?.id == idAlumno }
-        if (position > -1) {
-            try {
-                evaluaciones[evaluacion.toInt()][position] = nota
-                return true
-            }  catch (_:Exception) { return false }
-        } else return false
-    }
-
-    fun calculaEvaluacionFinal(){
-
+    //Comprueba que una evaluación introducida cumple con las restricciones.
+    private fun checkEvaluacion(evaluacion: String): Boolean {
+        try {
+            return evaluacion.toInt() in 0..3
+        } catch (_: Exception) {
+            return false
+        }
     }
 }
 
